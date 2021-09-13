e238df8b6f901bfa88b45c4e01895a64507c1172
itvadmin <itvadmin@LAPTOP1131>
2021-09-13 13:59:49 +0200

    matches the assignment

---
+++ Notitions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Notitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
